a55adefbe31d536f4816531486e741a06744d725
rajibtopuphouse <rstopuphouse@gmail.com>
2026-06-26 12:10:43 +0600

    Add Home component for Free Fire Top-Up page

---
+++ src/app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ textAlign: 'center', padding: '50px', background: '#000', color: '#fff', minHeight: '100vh' }}>
+      <h1>🔥 Free Fire Top-Up</h1>
+      <p>সরাসরি ডায়মন্ড কিনুন!</p>
+    </div>
+  );
+}
